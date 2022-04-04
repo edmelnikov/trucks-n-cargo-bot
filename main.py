eeab8684a102773ff90b4db4ade99e32fed30a59
edmelnikov <edmelnikov1@yandex.ru>
2022-04-04 16:43:51 +0300

    ati.su parser prototype wip

---
+++ main.py
@@ -0,0 +1,34 @@
+# bs4, selenium, requests
+import site_parser
+import time
+
+
+def main():
+    # parser = site_parser.Ati_truck_parser()
+    #
+    # parser.make_new_query(city_from="Амстердам",
+    #                       city_to="Москва",
+    #                       weight_t=None,
+    #                       volume_m3=None,
+    #                       cargo_type=None
+    #                       )
+    # # parser.make_new_query()
+    # time.sleep(1)
+    # parser.load_next_page()
+    # time.sleep(5)
+    # del parser
+
+    parser = site_parser.Ati_cargo_parser()
+    parser.make_new_query(city_from="Москва", city_to="Нижний Новгород")
+
+    parser.get_listing_data()
+    # parser.get_listing_data()
+
+    del parser
+
+
+if __name__ == '__main__':
+    main()
+
+
+
