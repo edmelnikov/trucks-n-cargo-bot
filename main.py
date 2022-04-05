475bb8341c3fe7c274d81cc340f96abebe5355f3
edmelnikov <edmelnikov1@yandex.ru>
2022-04-05 13:00:46 +0300

    AtiCargoParser prototype done

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # bs4, selenium, requests
 import site_parser
-
+import time
 
 def main():
     # parser = site_parser.Ati_truck_parser()
@@ -17,12 +17,19 @@ def main():
     # time.sleep(5)
     # del parser
 
-    parser = site_parser.Ati_cargo_parser()
-    parser.make_new_query(city_from="Москва", city_to="Нижний Новгород")
-
-    parser.get_listing_data()
-    # parser.get_listing_data()
-
+    # Example of AtiCargoParser usage
+    parser = site_parser.AtiCargoParser(delay_time=5)  # create an object
+    num_listings = parser.make_new_query(city_from="Москва", city_to="Нижний Новгород", max_weight=10)  # make a query
+    if num_listings > 0:  # if there are listings matching the query, get the data
+        print(parser.get_listing_data())  # get data from current page
+        if parser.load_next_page():  # load next page
+            print(parser.get_listing_data())  # get data from the next page
+        # and so on and so forth...
+        if parser.load_next_page():
+            print(parser.get_listing_data())
+        if parser.load_next_page():
+            print(parser.get_listing_data())
+        # time.sleep(5)
     del parser
 
 
--- site_parser.py
+++ site_parser.py
@@ -1,17 +1,24 @@
 from bs4 import BeautifulSoup
 import requests
 import urllib.request
-from selenium import webdriver
 import time
 import re
 from lxml import etree
 
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException, ElementNotInteractableException
+
 
 # ati.su listings parser (WIP) #
 # Abstract parser class #
-class Ati_parser():
+class AtiParser:
+    delay_time = 5
 
-    def __init__(self, url):
+    def __init__(self, url, delay_time):
+        self.delay_time = delay_time
         self.driver = webdriver.Chrome()  # create Chrome browser driver
         self.driver.get(url)
 
@@ -20,10 +27,26 @@ class Ati_parser():
         pass
 
     def fill_city_field(self, field_path, city, dropdown_path):  # enter city to specified text field
-        self.driver.find_element_by_xpath(field_path).send_keys(city)  # fill up departure city
-        self.driver.find_element_by_xpath(field_path).click()  # click to open up a dropdown list with suggested cities
-        time.sleep(1)  # wait for a dropdown list to load
-        self.driver.find_element_by_xpath(dropdown_path).click()  # click the first city in the dropdown
+        # self.driver.find_element(By.XPATH, field_path).send_keys(city)
+        # self.driver.find_element(By.XPATH, field_path).click()
+
+        # wait and check if the fields are present
+        try:
+            presence_of_elem = EC.presence_of_element_located((By.XPATH, field_path))
+            WebDriverWait(self.driver, self.delay_time).until(presence_of_elem).send_keys(city)  # fill up departure city
+
+            presence_of_elem = EC.presence_of_element_located((By.XPATH, field_path))
+            WebDriverWait(self.driver, self.delay_time).until(presence_of_elem).click()  # click to open up a dropdown list with suggested cities
+
+        except TimeoutException:
+            print("City input fields were not found!")
+
+        # wait and check if the dropdown is present
+        try:
+            presence_of_elem = EC.presence_of_element_located((By.XPATH, dropdown_path))
+            WebDriverWait(self.driver, self.delay_time).until(presence_of_elem).click()  # click the first city in the dropdown
+        except TimeoutException:
+            print("City dropdown was not found!")
 
     # Virtual methods #
     def make_new_query(self):
@@ -41,7 +64,7 @@ class Ati_parser():
 
 
 # WIP, on hold #
-class Ati_truck_parser(Ati_parser):
+class AtiTruckParser(AtiParser):
     domain = 'https://trucks.ati.su/'
 
     search_button_path = '//*[@id="root"]/div/div[1]/main/div[4]/div[2]/div[2]/button'  # "search" button path
@@ -52,14 +75,11 @@ class Ati_truck_parser(Ati_parser):
     next_page_button_path = '//*[@id="root"]/div/div[1]/main/div[5]/div/div[5]/div[1]/div/span[last()]'
     dropdown_first_path = '//*[@id="react-autowhatever-from--item-0"]'  # path of a first item in a dropdown list
 
-
-
     # Create Chrome driver open up the website #
     def __init__(self, sleep_time=2):
         super().__init__(self.domain)  # start chrome driver and open the website
 
-        self.sleep_time=sleep_time  # waiting time to let the site fully load (may be removed later)
-
+        self.sleep_time = sleep_time  # waiting time to let the site fully load (may be removed later)
         # query parameters
         self.city_from = None  # departure city
         self.city_to = None  # destination city
@@ -135,29 +155,42 @@ class Ati_truck_parser(Ati_parser):
         self.driver.find_element_by_xpath(self.next_page_button_path).click()
 
 
-class Ati_cargo_parser(Ati_parser):
-    domain='https://loads.ati.su/'
-
+class AtiCargoParser(AtiParser):
+    domain = 'https://loads.ati.su/'
     search_button_path = '//*[@id="search-scroll-anchor"]/div[2]/div[2]/button'  # "search" button path
     from_input_path = '//*[@id="__next"]/div/main/div[1]/div/div[3]/div[2]/div[1]/div[1]/div[1]/div/div/div/label/div[1]/input'  # departure point field path
     to_input_path = '//*[@id="__next"]/div/main/div[1]/div/div[3]/div[2]/div[2]/div[1]/div[1]/div/div/div/label/div[1]/input'
     dropdown_first_path = '//*[@id="react-autowhatever-1--item-0"]'  # path of a first item in a dropdown list
+    min_weight_path = '//*[@id="__next"]/div/main/div[1]/div/div[3]/div[2]/div[3]/div/div/div[1]/label/input'
+    max_weight_path = '//*[@id="__next"]/div/main/div[1]/div/div[3]/div[2]/div[3]/div/div/div[2]/label/input'
+    max_volume_path = '//*[@id="__next"]/div/main/div[1]/div/div[3]/div[2]/div[4]/div/div/div[2]/label/input'
+    next_page_button_path = '//*[@id="__next"]/div/main/div[1]/div/div[7]/div/span[last()]'
 
-    def __init__(self):
-        super().__init__(self.domain)  # start chrome driver and open the website
+    def __init__(self, delay_time):
+        super().__init__(self.domain, delay_time=5)  # start chrome driver and open the website
 
         self.city_from = None  # departure city
         self.city_to = None  # destination city
-
+        self.date_from = None  # starting weight
+        self.date_range = None
+        self.max_weight = None  # max load capacity in tonnes
+        self.weight_range = None  # load capacity range in tonnes
+        self.volume = None  # body capacity in m3
         self.total_listings_num = 0
 
+    # Form and make a new query #
+    # Returns a number of matching listings #
+    # Returns zero if either no listings have been found, or other problem has occurred #
     def make_new_query(self, city_from=None, city_to=None, date_from=None, date_range=None,
-                       max_weight_t=None, weight_range=None, volume_m3=None):
+                       max_weight=None, weight_range=None, volume=None):
 
         self.city_from = city_from
         self.city_to = city_to
-        #self.weight_t = weight_t
-        #self.volume_m3 = volume_m3
+        self.date_from = date_from
+        self.date_range = date_range
+        self.max_weight = max_weight
+        self.weight_range = weight_range
+        self.volume = volume
 
         self.total_listings_num = 0
 
@@ -169,24 +202,55 @@ class Ati_cargo_parser(Ati_parser):
         if city_to is not None:
             self.fill_city_field(self.to_input_path, self.city_to, self.dropdown_first_path)
 
-        # search for listings
-        self.driver.find_element_by_xpath(self.search_button_path).click()  # click "search" button
-        time.sleep(2)  # sleep to let the page fully load
+        # set dates
+        pass
+
+        # set weight (I know that I violate the DRY principle here, but who cares)
+        if type(self.weight_range) is tuple and len(self.weight_range) == 2:
+            try:
+                presence_of_elem = EC.presence_of_element_located((By.XPATH, self.min_weight_path))
+                WebDriverWait(self.driver, self.delay_time).until(presence_of_elem).send_keys(self.weight_range[0])
+
+                presence_of_elem = EC.presence_of_element_located((By.XPATH, self.max_weight_path))
+                WebDriverWait(self.driver, self.delay_time).until(presence_of_elem).send_keys(self.weight_range[1])
+            except TimeoutException:
+                print("Weight fields were not found!")
+        elif self.max_weight is not None:
+            try:
+                presence_of_elem = EC.presence_of_element_located((By.XPATH, self.max_weight_path))
+                WebDriverWait(self.driver, self.delay_time).until(presence_of_elem).send_keys(self.max_weight)
+            except TimeoutException:
+                print("Weight fields were not found!")
+
+        # set volume
+        if self.volume is not None:
+            try:
+                presence_of_elem = EC.presence_of_element_located((By.XPATH, self.max_volume_path))
+                WebDriverWait(self.driver, self.delay_time).until(presence_of_elem).send_keys(self.volume)
+            except TimeoutException:
+                print("Volume fields were not found!")
+
+        # click search button
+        try:
+            presence_of_elem = EC.presence_of_element_located((By.XPATH, self.search_button_path))
+            WebDriverWait(self.driver, self.delay_time).until(presence_of_elem).click()  # fill up departure city
+        except TimeoutException:
+            print("Search button was not found!")
+            return 0  # we can't progress any further, return 0 meaning that no listings were found
 
         # parse the page with listings and find the total number of available listings
         soup = BeautifulSoup(self.driver.page_source, "html.parser")  # get page source with all listings
-
         total_listing_num_div = soup.find('div', {'class': 'SearchResults_searchStatus__n103h'})  # div containing total number of listings
         if total_listing_num_div is not None:
-            listing_num_str = total_listing_num_div.find('h2', {'class': 'glz-h glz-is-size-2'}).getText()
+            listing_num_str = total_listing_num_div.find('h2', {'class': 'glz-h glz-is-size-2'}).getText()  # unsafe, may cause problems
             self.total_listings_num = int(re.findall(r'\d+', listing_num_str)[0])
         else:
             self.total_listings_num = 0
         return self.total_listings_num
 
-
     # Get listing data in form of a dictionary from current page #
-    # this code ignores tenders! #
+    # This code ignores "tenders"! #
+    # Returns a list of dictionaries for each listing on current page #
     def get_listing_data(self):
         soup = BeautifulSoup(self.driver.page_source, "html.parser")
         listing_divs = soup.find_all('div', {'class': '_39xB9'})  # find divs containing listings
@@ -220,13 +284,22 @@ class Ati_cargo_parser(Ati_parser):
                 listing_datum['transport_type'] = transport_data_div.find('span', {'class': 'glz-tooltiptext'}).getText(separator=' ')
 
                 listing_data.append(listing_datum)
-        print(listing_data)
         return listing_data
 
     # Load next page, if it exists #
-    # #
+    # Returns True if the next page has been successfully loaded #
+    # Returns False otherwise #
     def load_next_page(self):
-        pass
+        try:
+            presence_of_elem = EC.presence_of_element_located((By.XPATH, self. next_page_button_path))
+            WebDriverWait(self.driver, self.delay_time).until(presence_of_elem).click()  # click "next page" button
+            time.sleep(1)  # a small delay to let the page update (temporary solution, has to be changed later)
+            return True
+        except TimeoutException:
+            print("Next page button was not found!")
+            return False
+        except ElementNotInteractableException:  # last page and the next page button is not clickable
+            return False
 
 # avito.ru listings parser #
 # def search_by_query_avito(query, city, num_listings=10):
